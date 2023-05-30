905b8a741f5ec414866f8db31a6f05fe6f7f27c1
juliawiktoria <julia.w.zieba@gmail.com>
2023-05-30 14:42:17 +0100

    completed perfect numbers exercise

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -4,5 +4,15 @@ enum class Classification {
 }
 
 fun classify(naturalNumber: Int): Classification {
+
+    if (naturalNumber < 1) throw RuntimeException()
+
+    val divisors = (1..naturalNumber - 1).filter { naturalNumber % it == 0 }.toMutableList()
+
+    println(divisors)
+
+    if (divisors.sum() == naturalNumber) return Classification.PERFECT
+    if (divisors.sum() > naturalNumber) return Classification.ABUNDANT
+
     return Classification.DEFICIENT
 }
