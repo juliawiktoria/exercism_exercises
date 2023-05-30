3db03f1761d66234006d09cb93f622b7740e2c22
juliawiktoria <julia.w.zieba@gmail.com>
2023-05-30 13:20:27 +0100

    small change

--- resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -4,7 +4,7 @@ object ResistorColorTrio {
 
     fun text(vararg input: Color): String {
         val number = "${input[0].ordinal}${input[1].ordinal}${"0".repeat(input[2].ordinal)}".toInt()
-        val level = (number.toString().length - 1) / 3
+        val level = (number.toString().length - 1) / 3.0
         val trimmedValue = number / 1_000.0.pow(level)
         val unit = Unit.values()[level].name
         return "${trimmedValue.toInt()} ${unit.toLowerCase()}"
