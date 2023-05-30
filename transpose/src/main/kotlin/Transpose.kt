550a38717f6ce5d073643be26d003c22776c3f1c
juliawiktoria <julia.w.zieba@gmail.com>
2023-05-30 13:59:38 +0100

    completed the transpose exercise

--- transpose/src/main/kotlin/Transpose.kt
+++ transpose/src/main/kotlin/Transpose.kt
@@ -1,6 +1,15 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
+        val row: Int = input.maxOfOrNull { it.length } ?: 0
+        var result = Array<String>(row) { "" }
+
+        for(i in 0 until row){
+            val col = input.indexOfLast { it.length > i }
+            for(j in 0..col){
+                result[i] += input[j].elementAtOrElse(i) { ' ' }.toString()
+            }
+        }
+        return result.toList()
     }
 }
