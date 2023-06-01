7ab5230bbd480ecb8ee6cf85a526df65633443d7
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-01 11:28:34 +0100

    completed collatz conjecture exercise

--- collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,5 +1,19 @@
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+        if (start < 1) throw IllegalArgumentException()
+        if (start == 1) return 0
+        var number = start
+        var steps = 0
+
+        while (number != 1) {
+            steps++
+            if (number % 2 == 0) {
+                number = number / 2
+            } else {
+                number = 3 * number + 1
+            }
+        }
+
+        return steps
     }
 }
