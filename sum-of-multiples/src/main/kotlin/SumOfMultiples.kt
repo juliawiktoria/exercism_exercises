f81a0596f81daf5d6087609e43a52fc5211553db
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 11:25:36 +0100

    completed sum of multiples exercise

--- sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,6 +1,9 @@
 object SumOfMultiples {
-
-    fun sum(factors: Set<Int>, limit: Int): Int {
-        TODO("Implement this function to complete the task")
+    fun sum(factors: Set<Int>, limit: Int) =
+        (1 until limit)
+            .filter { factors.hasInt(it) }
+            .sum()
+    private fun Set<Int>.hasInt(n: Int) = any {
+        it > 0 && n % it == 0
     }
 }
