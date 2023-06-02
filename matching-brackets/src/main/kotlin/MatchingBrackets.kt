012f5e92e4e787c68354470ab256395578aef22e
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 15:44:38 +0100

    completed matching brackets exercise

--- matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -1,6 +1,23 @@
 object MatchingBrackets {
+    private val openingBrackets = listOf('(', '[', '{')
+    private val closingBrackets = listOf(')', ']', '}')
+    private val bracketPairs = mapOf(')' to '(', ']' to '[', '}' to '{')
 
     fun isValid(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val stack = mutableListOf<Char>()
+
+        for (char in input) {
+            if (openingBrackets.contains(char)) {
+                stack.add(char)
+            } else if (closingBrackets.contains(char)) {
+                if (stack.isEmpty() || stack.last() != bracketPairs[char]) {
+                    return false
+                } else {
+                    stack.removeAt(stack.size - 1)
+                }
+            }
+        }
+
+        return stack.isEmpty()
     }
 }
