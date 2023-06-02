aad30024fec510ecc95c45aa52b717090c3aacd3
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 15:00:06 +0100

    completed linked list exercise

--- linked-list/src/main/kotlin/LinkedList.kt
+++ linked-list/src/main/kotlin/LinkedList.kt
@@ -1,18 +1,62 @@
 class Deque<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+
+    inner class Node<T>(val value: T) {
+        var next: Node<T>? = null
+        var prev: Node<T>? = null
+    }
 
     fun push(value: T) {
-        TODO("Implement this function to complete the task")
+        val newNode = Node(value)
+
+        if (head == null) {
+            head = newNode
+            tail = newNode
+        } else {
+            newNode.prev = tail
+            tail?.next = newNode
+            tail = newNode
+        }
     }
 
     fun pop(): T? {
-        TODO("Implement this function to complete the task")
+        val poppedValue = tail?.value
+        tail = tail?.prev
+
+        if (tail == null) {
+            head = null
+        } else {
+            tail?.next = null
+        }
+
+        return poppedValue
     }
 
     fun unshift(value: T) {
-        TODO("Implement this function to complete the task")
+        val newNode = Node(value)
+
+        if (head == null) {
+            head = newNode
+            tail = newNode
+        } else {
+            newNode.next = head
+            head?.prev = newNode
+            head = newNode
+        }
     }
 
     fun shift(): T? {
-        TODO("Implement this function to complete the task")
+        val shiftedValue = head?.value
+        head = head?.next
+
+        if (head == null) {
+            tail = null
+        } else {
+            head?.prev = null
+        }
+
+        return shiftedValue
     }
 }
+
