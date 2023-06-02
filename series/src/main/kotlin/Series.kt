023bc8b46f093888bea1595aff627194eecdc572
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 15:57:50 +0100

    completed series exercise

--- series/src/main/kotlin/Series.kt
+++ series/src/main/kotlin/Series.kt
@@ -1,6 +1,20 @@
 object Series {
-
     fun slices(n: Int, s: String): List<List<Int>> {
-        TODO("Implement this function to complete the task")
+        if (n > s.length || n < 1) {
+            throw IllegalArgumentException()
+        }
+        val substrings = mutableListOf<List<Int>>()
+
+        if (n > s.length) {
+            return substrings
+        }
+
+        for (i in 0..s.length - n) {
+            val substring = s.substring(i, i + n)
+            val digits = substring.map { it.toString().toInt() }
+            substrings.add(digits)
+        }
+
+        return substrings
     }
 }
