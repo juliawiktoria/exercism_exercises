f329e77133560d8d0227c6e2803d5094811bcb95
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 12:48:06 +0100

    completed the bob exercise

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,20 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        val prompt = input.replace("\\s".toRegex(), "")
+        return when {
+            prompt.isYelling() && prompt.isQuestion() -> "Calm down, I know what I'm doing!"
+            prompt.isYelling() -> "Whoa, chill out!"
+            prompt.isQuestion() -> "Sure."
+            prompt.isSilence() -> "Fine. Be that way!"
+            else -> "Whatever."
+        }
     }
+
+    private fun String.isQuestion(): Boolean = endsWith("?")
+    private fun String.isSilence(): Boolean = isBlank()
+    private fun String.isYelling(): Boolean {
+        return !letters().isSilence() and letters().isUpperCase()
+    }
+    private fun String.isUpperCase(): Boolean  = all(Char::isUpperCase)
+    private fun String.letters(): String  = filter(Char::isLetter)
 }
