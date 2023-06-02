587550d14d1fa50d148a2633ba7c029d100e00b7
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 15:27:17 +0100

    completed darts exercise

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,11 @@
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Number, y: Number): Int {
+        val distance = Math.sqrt(x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble())
+
+        if (distance <= 1) return 10
+        if (distance <= 5) return 5
+        if (distance <= 10) return 1
+        return 0
     }
 }
