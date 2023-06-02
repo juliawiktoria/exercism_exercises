5830cc86c23fdb52cd31807ff61adab086dea00d
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-02 14:48:30 +0100

    completed pascal triangle exercise

--- pascals-triangle/src/main/kotlin/PascalsTriangle.kt
+++ pascals-triangle/src/main/kotlin/PascalsTriangle.kt
@@ -1,6 +1,24 @@
 object PascalsTriangle {
 
     fun computeTriangle(rows: Int): List<List<Int>> {
-        TODO("Implement this function to complete the task")
+        val triangle = mutableListOf<List<Int>>()
+
+        for (row in 0 until rows) {
+            val currentRow = mutableListOf<Int>()
+            currentRow.add(1)
+
+            for (col in 1 until row) {
+                val value = triangle[row - 1][col - 1] + triangle[row - 1][col]
+                currentRow.add(value)
+            }
+
+            if (row > 0) {
+                currentRow.add(1)
+            }
+
+            triangle.add(currentRow)
+        }
+
+        return triangle
     }
 }
