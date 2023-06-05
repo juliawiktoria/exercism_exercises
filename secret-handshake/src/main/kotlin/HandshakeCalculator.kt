fe367bcf076ef0134649707526bc64f692696cfc
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-05 15:44:58 +0100

    completed the secret handshake exercise

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,5 +1,15 @@
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
-        TODO("Implement the function to complete the task")
+        val binaryDigits = Integer.toBinaryString(number).reversed()
+
+        val handshake = mutableListOf<Signal>()
+
+        if (binaryDigits[0] == '1') handshake.add(Signal.WINK)
+        if (binaryDigits.length > 1 && binaryDigits[1] == '1') handshake.add(Signal.DOUBLE_BLINK)
+        if (binaryDigits.length > 2 && binaryDigits[2] == '1') handshake.add(Signal.CLOSE_YOUR_EYES)
+        if (binaryDigits.length > 3 && binaryDigits[3] == '1') handshake.add(Signal.JUMP)
+        if (binaryDigits.length > 4 && binaryDigits[4] == '1') handshake.reverse()
+
+        return handshake
     }
 }
