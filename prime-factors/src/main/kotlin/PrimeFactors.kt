6b63272e420f6d843b7df1284fed269fb73c0f4a
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-07 11:56:30 +0100

    completed prime factors exercise

--- prime-factors/src/main/kotlin/PrimeFactors.kt
+++ prime-factors/src/main/kotlin/PrimeFactors.kt
@@ -1,10 +1,36 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        TODO("Implement this function to complete the task")
+        val primeFactors = mutableListOf<Int>()
+        var n = int
+        var divisor = 2
+
+        while (divisor <= n) {
+            if (n % divisor == 0) {
+                primeFactors.add(divisor)
+                n /= divisor
+            } else {
+                divisor++
+            }
+        }
+
+        return primeFactors
     }
 
     fun primeFactors(long: Long): List<Long> {
-        TODO("Implement this function to complete the task")
+        val primeFactors = mutableListOf<Long>()
+        var n = long
+        var divisor = 2.toLong()
+
+        while (divisor <= n) {
+            if (n % divisor == 0.toLong()) {
+                primeFactors.add(divisor)
+                n /= divisor
+            } else {
+                divisor++
+            }
+        }
+
+        return primeFactors
     }
 }
