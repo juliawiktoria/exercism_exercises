09c896bdd72bcd010ee1913fbeb6955bf0f4f152
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-15 16:21:27 +0100

    solved dominoes exercise

--- dominoes/src/main/kotlin/Dominoes.kt
+++ dominoes/src/main/kotlin/Dominoes.kt
@@ -1,10 +1,24 @@
-class ChainNotFoundException(msg: String) : RuntimeException(msg)
-
+class ChainNotFoundException : RuntimeException()
 data class Domino(val left: Int, val right: Int)
-
 object Dominoes {
-
-    fun formChain(inputDominoes: List<Domino>): List<Domino> {
-        TODO("Implement this to complete the task")
+    fun formChain(vararg xs: Domino) = formChain(xs.toList())
+    fun formChain(xs: List<Domino>): List<Domino> =
+        if (xs.isEmpty())
+            emptyList()
+        else {
+            val ys = mutableListOf(listOf(xs.first()))
+            go(ys, xs.drop(1))
+            ys.removeIf { it.size != xs.size || it.first().left != it.last().right }
+            if (ys.isEmpty()) throw ChainNotFoundException() else ys.first()
+        }
+    private fun go(acc: MutableList<List<Domino>>, pool: List<Domino>) {
+        if (pool.isEmpty()) return
+        val xs = acc.last()
+        val last = xs.last()
+        pool.filter { last.right == it.left || last.right == it.right }
+            .forEach {
+                acc.add(xs.plus(if (last.right == it.left) it else Domino(it.right, it.left)))
+                go(acc, pool.minus(it))
+            }
     }
 }
