cdaf5ed08b9849733e0045481455480c1af0dce6
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-15 09:28:38 +0100

    completed binary search exercise

--- binary-search/src/main/kotlin/BinarySearch.kt
+++ binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,22 @@
 object BinarySearch {
     fun search(list: List<Int>, item: Int): Int {
-        TODO("Implement the function to complete the task. Change the signature if necessary.")
+        if (list.isEmpty()) throw NoSuchElementException()
+
+        if(item < list.first() || item > list.last()) throw NoSuchElementException()
+
+        var low = 0
+        var high = list.size - 1
+
+        while (low <= high) {
+            val mid = (low + high) / 2
+
+            when {
+                list[mid] == item -> return mid
+                list[mid] < item -> low = mid + 1
+                else -> high = mid - 1
+            }
+        }
+
+        throw NoSuchElementException()
     }
 }
