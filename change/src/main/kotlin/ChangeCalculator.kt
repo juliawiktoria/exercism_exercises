b4bf5d05ddaa81f6e762c43a3628f7294e417a1d
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-15 11:05:24 +0100

    solved change exercise

--- change/src/main/kotlin/ChangeCalculator.kt
+++ change/src/main/kotlin/ChangeCalculator.kt
@@ -1,7 +1,17 @@
-class ChangeCalculator {
-    // TODO: implement proper constructor
-
-    fun computeMostEfficientChange(grandTotal: Int): List<Int> {
-        TODO("Implement the function to complete the task")
+class ChangeCalculator(val coins: List<Int>) {
+    private val sortedCoins = coins.sorted()
+    fun computeMostEfficientChange(total: Int): List<Int> {
+        require(total >= 0) { "Negative totals are not allowed." }
+        val changes = (1..total).fold(listOf<List<Int>?>(listOf<Int>())) { acc, amount ->
+            coins
+                .filter { acc.getOrNull(amount - it) != null }
+                .map { listOf(it) + acc.get(amount - it)!! }
+                .sortedBy { it.size }
+                .firstOrNull()
+                .let { acc.plusElement(it) }
+        }
+        return requireNotNull(changes.lastOrNull()) {
+            "The total $total cannot be represented in the given currency."
+        }
     }
 }
