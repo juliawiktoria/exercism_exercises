4ea37aedc10d2de004e90940af26e5a81b9afe4d
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-15 15:44:31 +0100

    completed meetup exercise

--- meetup/src/main/kotlin/Meetup.kt
+++ meetup/src/main/kotlin/Meetup.kt
@@ -1,11 +1,18 @@
 import java.time.DayOfWeek
 import java.time.LocalDate
-
-class Meetup {
-
-    // TODO: implement proper constructor
-
-    fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
-        TODO("Implement this function to complete the task")
+class Meetup(val month: Int, val year: Int) {
+    fun day(dow: DayOfWeek, sched: MeetupSchedule): LocalDate =
+        daysInMonth.filter { it.dayOfWeek == dow }.let { days ->
+            when(sched) {
+                MeetupSchedule.FIRST -> days.first()
+                MeetupSchedule.LAST -> days.last()
+                MeetupSchedule.SECOND -> days.drop(1).first()
+                MeetupSchedule.THIRD -> days.drop(2).first()
+                MeetupSchedule.FOURTH -> days.drop(3).first()
+                MeetupSchedule.TEENTH -> days.find { it.dayOfMonth > 12 }!!
+            }
+        }
+    val daysInMonth = generateSequence(LocalDate.of(year, month, 1)) {
+        it.plusDays(1).let { day -> if (day.month.value != month) null else day }
     }
 }
