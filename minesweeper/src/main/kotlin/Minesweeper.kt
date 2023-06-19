17b435b24cb7cfc7084ff8636e8b6d2a86353e12
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-19 12:24:47 +0100

    solved Minesweeper exercise

--- minesweeper/src/main/kotlin/Minesweeper.kt
+++ minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,8 +1,22 @@
-data class MinesweeperBoard(val todo: Nothing) {
-
-    // TODO: Implement proper constructor
-
-    fun withNumbers(): List<String> {
-        TODO("Implement this function to complete the task")
+class MinesweeperBoard(val inputBoard: List<String>) {
+    fun withNumbers(): List<String> =
+        inputBoard.mapIndexed { row, str ->
+            str.mapIndexed { col, cell -> when (cell) {
+                ' ' -> adjacentMines(row, col)
+                else -> cell
+            }
+            }.joinToString("")
+        }
+    private fun adjacentMines(row: Int, col: Int): Char {
+        val count =
+            (Math.max(0, row - 1)..Math.min(inputBoard.size - 1, row + 1))
+                .flatMap { i ->
+                    (Math.max(0, col - 1)..Math.min(inputBoard[i].length - 1, col + 1))
+                        .map { j ->
+                            if (inputBoard[i][j] == '*') 1 else 0
+                        }
+                }
+                .sum()
+        return if (count > 0) (count + 48).toChar() else ' '
     }
 }
