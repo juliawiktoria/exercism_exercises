cfee094f30a4c93dab2d479e5f51615b75cb2919
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-21 09:42:47 +0100

    solved clock exercise

--- clock/src/main/kotlin/Clock.kt
+++ clock/src/main/kotlin/Clock.kt
@@ -1,13 +1,27 @@
-class Clock {
-    //TODO: implement proper constructor
-
-    //TODO: find a convenient way to take over `toString()` and `equals()`
-
+class Clock(hh: Int, mm: Int) {
+    private var hh = 0
+    private var mm = 0
+    init {
+        setup(hh, mm)
+    }
     fun subtract(minutes: Int) {
-        TODO("Implement the function to complete the task")
+        add(-minutes)
+    }
+    fun add(minutes: Int) {
+        setup(hh, mm + minutes)
+    }
+    private fun setup(hh: Int, mm: Int) {
+        this.hh = ((hh + mm / 60) % 24)
+            .let { (if (mm % 60 < 0) it - 1 else it) }
+            .let { if (it < 0) it + 24 else it }
+        this.mm = (mm % 60)
+            .let { if (it < 0) it + 60 else it }
+    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other is Clock) return hh == other.hh && mm == other.mm
+        return false
     }
 
-    fun add(minutes: Int) {
-        TODO("Implement the function to complete the task")
-    }
+    override fun toString(): String = "%02d:%02d".format(hh, mm)
 }
