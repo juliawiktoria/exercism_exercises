8ca9ee13f67897fb543234504e4295ad504905ab
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-21 11:53:22 +0100

    completed spiral matrix exercise

--- spiral-matrix/src/main/kotlin/SpiralMatrix.kt
+++ spiral-matrix/src/main/kotlin/SpiralMatrix.kt
@@ -1,6 +1,17 @@
 object SpiralMatrix {
-
     fun ofSize(size: Int): Array<IntArray> {
-        TODO("Implement this function to complete the task")
+        val grid = Array(size, { IntArray(size) })
+        var count = 0
+        var start = 0
+        var end = size - 1
+        while (start <= end) {
+            (start..end).forEach { grid[start][it] = ++count }
+            (start + 1..end).forEach { grid[it][end] = ++count }
+            (end - 1 downTo start).forEach { grid[end][it] = ++count }
+            (end - 1 downTo start + 1).forEach { grid[it][start] = ++count }
+            ++start
+            --end
+        }
+        return grid
     }
 }
