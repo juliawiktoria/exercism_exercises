fa063ebcb44364bbaa0df0fa10790917d3d98c32
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-21 14:01:30 +0100

    solved grade school exercise

--- grade-school/src/main/kotlin/GradeSchool.kt
+++ grade-school/src/main/kotlin/GradeSchool.kt
@@ -1,14 +1,6 @@
 class School {
-
-    fun add(student: String, grade: Int) {
-        TODO("Implement this function to complete the task")
-    }
-
-    fun grade(grade: Int): List<String> {
-        TODO("Implement this function to complete the task")
-    }
-
-    fun roster(): List<String> {
-        TODO("Implement this function to complete the task")
-    }
+    private val students = mutableMapOf<Int, MutableList<String>>()
+    fun add(student: String, grade: Int) = students.getOrPut(grade) { mutableListOf() }.add(student)
+    fun grade(grade: Int): List<String> = students[grade]?.sorted() ?: emptyList()
+    fun roster(): List<String> = students.toList().sortedBy { it.first }.flatMap { it.second.sorted() }
 }
