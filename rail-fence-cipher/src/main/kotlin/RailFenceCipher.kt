7253cf68f13b0bf97cce0141dd7278c7c7e48faf
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-22 09:03:59 +0100

    completed rail fence cipher exercise

--- rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
+++ rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
@@ -1,12 +1,26 @@
-class RailFenceCipher {
-
-    // TODO: Implement proper constructor
-
-    fun getEncryptedData(input: String): String {
-        TODO("Implement this function to complete the task")
-    }
-
-    fun getDecryptedData(input: String): String {
-        TODO("Implement this function to complete the task")
+class RailFenceCipher(private val rails: Int) {
+    fun getEncryptedData(plainText: String): String =
+        plainText.asSequence().zip(tracks.take(plainText.length))
+            .sortedBy { it.second }
+            .map { it.first }
+            .joinToString("")
+    fun getDecryptedData(cipherText: String): String =
+        tracks.take(cipherText.length)
+            .mapIndexed { index, track -> index to track }
+            .toList()
+            .sortedBy { it.second }
+            .map { it.first }
+            .zip(cipherText.asIterable())
+            .sortedBy { it.first }
+            .map { it.second }
+            .joinToString("")
+    private val tracks = sequence {
+        var current = 0
+        var up = true
+        while (true) {
+            yield(current)
+            current = if (up) current + 1 else current - 1
+            up = if (up) current < rails - 1 else current == 0
+        }
     }
 }
