3304dccc90316f3735dd182dbf36ea5e86e0808c
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-27 14:51:19 +0100

    solved Atbash cipher exercise

--- atbash-cipher/src/main/kotlin/Atbash.kt
+++ atbash-cipher/src/main/kotlin/Atbash.kt
@@ -1,10 +1,10 @@
 object Atbash {
-
-    fun encode(s: String): String{
-        TODO("Implement the function to complete the task")
-    }
-
-    fun decode(s: String): String{
-        TODO("Implement the function to complete the task")
-    }
+    private val MAPPINGS = (('a'..'z').zip('z'.downTo('a')) + ('0'..'9').zip('0'..'9')).toMap()
+    fun encode(input: String) =
+        input.toLowerCase().replace("[^a-z0-9]".toRegex(), "")
+            .translate()
+            .chunked(5)
+            .joinToString(" ")
+    fun decode(input: String) = input.replace(" ", "").translate()
+    private fun String.translate() = this.fold(StringBuilder()) { sb, c -> sb.append(MAPPINGS[c]) }.toString()
 }
