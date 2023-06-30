6c91d3f0709129d3d9a014de4a08618affd70dfd
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-30 11:08:10 +0100

    completed DiffieHellman exercise

--- diffie-hellman/src/main/kotlin/DiffieHellman.kt
+++ diffie-hellman/src/main/kotlin/DiffieHellman.kt
@@ -1,16 +1,21 @@
 import java.math.BigInteger
+import kotlin.random.Random
 
 object DiffieHellman {
 
     fun privateKey(prime: BigInteger): BigInteger {
-        TODO("Implement the function to complete the task")
+        if(prime.signum() != 1) throw IllegalArgumentException("")
+        if(prime.compareTo(BigInteger(Int.MAX_VALUE.toString())) == 1){
+            return BigInteger(prime.bitLength()-1, java.util.Random())
+        }else{
+            return BigInteger(Random.nextInt(1, prime.toInt()-1).toString())
+        }
     }
 
-    fun publicKey(p: BigInteger, g: BigInteger, privKey: BigInteger): BigInteger {
-        TODO("Implement the function to complete the task")
+    fun publicKey(p: BigInteger, g: BigInteger, privateKey: BigInteger): BigInteger {
+        return g.modPow(privateKey, p)
     }
-
     fun secret(prime: BigInteger, publicKey: BigInteger, privateKey: BigInteger): BigInteger {
-        TODO("Implement the function to complete the task")
+        return publicKey.modPow(privateKey, prime)
     }
 }
