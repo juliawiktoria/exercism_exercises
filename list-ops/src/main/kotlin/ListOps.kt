8d811fcbb072d15eacdebdf3497b144954efedb9
juliawiktoria <julia.w.zieba@gmail.com>
2023-06-30 11:12:09 +0100

    completed list ops exercise

--- list-ops/src/main/kotlin/ListOps.kt
+++ list-ops/src/main/kotlin/ListOps.kt
@@ -1,29 +1,44 @@
 fun <T> List<T>.customAppend(list: List<T>): List<T> {
-    TODO("Implement this function to complete the task")
+    return list.customFoldLeft(this) { newList, element -> newList + element }
 }
-
 fun List<Any>.customConcat(): List<Any> {
-    TODO("Implement this function to complete the task")
+    return customFoldLeft(listOf()) { acc, element ->
+        when (element) {
+            is List<*> -> acc + (element as List<Any>).customConcat()
+            else -> acc + element
+        }
+    }
 }
-
 fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> {
-    TODO("Implement this function to complete the task")
+    return customFoldLeft(this) { newList, element ->
+        if (predicate(element)) {
+            newList
+        } else newList - element
+    }
 }
-
-val List<Any>.customSize: Int get() = TODO("Implement this getter to complete the task")
-
+val List<Any>.customSize: Int get() = this.customFoldLeft(0) { acc, _ -> acc + 1 }
 fun <T, U> List<T>.customMap(transform: (T) -> U): List<U> {
-    TODO("Implement this function to complete the task")
+    return customFoldLeft(listOf()) { newList, element -> newList + transform(element) }
 }
-
 fun <T, U> List<T>.customFoldLeft(initial: U, f: (U, T) -> U): U {
-    TODO("Implement this function to complete the task")
+    if (this.isEmpty()) {
+        return initial
+    } else {
+        return drop(1).customFoldLeft(f(initial, this.first()), f)
+    }
 }
-
 fun <T, U> List<T>.customFoldRight(initial: U, f: (T, U) -> U): U {
-    TODO("Implement this function to complete the task")
+    if (this.isEmpty()) {
+        return initial
+    } else {
+        return f(this.first(), drop(1).customFoldRight(initial, f))
+    }
 }
-
 fun <T> List<T>.customReverse(): List<T> {
-    TODO("Implement this function to complete the task")
+    if (this.isEmpty()) {
+        return emptyList()
+    } else {
+        val initial: List<T> = listOf()
+        return customFoldLeft(initial) { newList, element -> listOf(element) + newList }
+    }
 }
